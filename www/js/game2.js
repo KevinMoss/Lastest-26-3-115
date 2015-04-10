3c0c8b8ee5928fec51a0b0b5f7dba32f0d425019
KevinMoss <kevinmoshey1@hotmail.com>
2015-04-10 12:26:24 +0100

    Update game2.js

--- www/js/game2.js
+++ www/js/game2.js
@@ -60,6 +60,60 @@ $("#input-form").submit(function(bin2hex){
 	alert("");
 	event.preventDefault();
 });
+<!-- Begin
+// Insert number of questions
+var numQues = 16;
+
+// Insert number of choices in each question
+var numChoi = 4;
+
+// Insert number of questions displayed in answer area
+var answers = new Array(16);
+
+// Insert answers to questions
+answers[0] = "MyFirstProgram";
+answers[1] = "Public Static Void Main(String[args])";
+answers[2] = "String word;";
+answers[3] = "for(i=0; i<1; i++)";
+answers[4] = "A Compiler";
+answers[5] = "System.out.println('Welcome to java')";
+answers[6] = "Test.java";
+answers[7] = "import javax.swing.JOptionPane;";
+answers[8] = "A semicolon (;)";
+answers[9] = "10";
+answers[10] = "100";
+answers[11] = "Braces";
+answers[12] = "x is 1";
+answers[13] = "CPU";
+answers[14] = "**Comment**";
+answers[15] = "input.nextInteger();";
+
+// Do not change anything below here ...
+function getScore(form) {
+  var score = 0;
+  var currElt;
+  var currSelection;
+  for (i=0; i<numQues; i++) {
+    currElt = i*numChoi;
+    for (j=0; j<numChoi; j++) {
+      currSelection = form.elements[currElt + j];
+      if (currSelection.checked) {
+        if (currSelection.value == answers[i]) {
+          score++;
+          break;
+        }
+      }
+    }
+  }
+  score = Math.round(score/numQues*100);
+  scor = Math.round(score + numQues-1);
+  form.percentage.value = score + "%";
+  var correctAnswers = "";
+  for (i=1; i<=numQues; i++) {
+    correctAnswers += i + ". " + answers[i-1] + "\r\n";
+  }
+  form.solutions.value = correctAnswers;
+}
 
 
 	
